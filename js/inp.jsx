4e031c3ec638dc3af07ec3f80dd7436349b42dbd
Klajdi <klajdizmalaj@icloud.com>
2021-01-14 10:13:10 +0100

    m

--- js/inp.jsx
+++ js/inp.jsx
@@ -2,20 +2,19 @@ import { Progress } from "antd";
 import axios from "axios";
 <input
   type="file"
-  onChange={(e) => {
+  onChange={async (e) => {
     //console.log("ca ka e", e.target.files);
     let data = new FormData();
     data.append("file", e.target.files[0]);
-    axios
-      .post("/", data, {
-        onUploadProgress: (e) => {
-          var percentComplete = parseInt((e.loaded * 100) / e.total);
-          console.log("ca ka progres", percentComplete);
-          this.setState({ prc: percentComplete });
-        },
-      })
-      .then((data) => data.json())
-      .then((res) => console.log("res", res));
+    var req = await axios.post("/", data, {
+      onUploadProgress: (e) => {
+        var percentComplete = parseInt((e.loaded * 100) / e.total);
+        console.log("ca ka progres", percentComplete);
+        this.setState({ prc: percentComplete });
+      },
+    });
+    let data = await req.json();
+    console.log("data", data);
   }}
 />;
 
