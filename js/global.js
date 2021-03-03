ace5a3f1fef9af802f64de71ab47179c3dc877b7
Klajdi Zmalaj <32370399+KlajdiZmalaj@users.noreply.github.com>
2021-03-03 11:47:48 +0100

    m

--- js/global.js
+++ js/global.js
@@ -155,11 +155,22 @@ $(document).ready(() => {
       });
     };
     //main Banner replace slider
-    $(".main-slider").html(
-      `<img onclick="mainBannerClick();" src="${$(
-        '.main-slider .slick-track img[src*="main-banner.jpg"]'
-      ).attr("src")}">`
-    );
+    $(".main-slider").slick("unslick");
+    if (btoa(window.location.host) === "Z29sZHNwb3J0LmxpdmU=") {
+      $("body").attr("host", "Z29sZHNwb3J0LmxpdmU=");
+      $(".main-slider").slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        slidesToShow: 1,
+      });
+    }
+    // $(".main-slider").html(
+    //   `<img onclick="mainBannerClick();" src="${$(
+    //     '.main-slider .slick-track img[src*="main-banner.jpg"]'
+    //   ).attr("src")}">`
+    // );
   }
   if (window.showCombination && window.showMultiple && window.showSingle) {
     //ku ka kupon ->
