8aee4992227782cd09cf78da6b1f2bc2e2b6e652
erjusstafa <erjus.stafa@gmail.com>
2021-06-02 02:21:06 -0700

    m

--- js/extra-bbgame.js
+++ js/extra-bbgame.js
@@ -9,13 +9,80 @@ $(document).ready(() => {
   );
 });
 
-/* carousel
-$(document).ready(function () {
-  $(".main-slider").slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    speed: 300,
-  });
-});
-*/
+
+
+
+
+
+
+
+
+//testim per JQUERY
+
+
+
+//  $(document).ready(function(){
+//   $(".bottom-header-content li:last-child").append(" <b>click</b>").css("cursor", "pointer")
+  
+//    });
+
+
+// $( ".bottom-header-content li:last-child" ).bind( "click", function() {
+//   alert( "User clicked on 'foo.'" );
+// });
+
+//or
+ 
+//   $(document).ready(function(){
+//    $(".bottom-header-content li:last-child").click(function(){
+//     alert("hello BBGAME");
+//    });
+//  });
+
+
+
+// $(document).ready(function(){    
+//   // $(".bottom-header-content li:last-child").mousemove(function(){    
+//   //     $(".bottom-header-content").toggle()
+      
+//       $(".bottom-header-content li:last-child").mouseover(function() { 
+//         $(".bottom-header-content").css('display','none'); 
+//       });
+
+//       $(".bottom-header-content li:last-child").mouseout(function() {
+//          $(".bottom-header-content").css('display','flex');
+//          });
+// }); 
+
+
+
+// $(document).ready(function(){  
+//       $( "#miniTablet_1_1" ).fadeIn(600,"linear");
+// }); 
+
+
+
+// $("#sportsNum").each(function() {
+//   $(this).bind("click", function() {
+//       $(this).dialog({
+//         modal: true,
+//         height: 590,
+//         width: 1005,
+//         autoOpen: true
+//       });
+//   });
+// });​​​​​​​​​​​​​​​​​​​
+
+
+
+// $( document ).ready(function() {
+//   $( ".single-block.closed" ).bind( "mouseover",hit );  //thirr funksionin e meposhtem
+// });
+
+// function hit (e){ 
+//   alert( "The mouse cursor is at (" +e.pageX + ", " + e.pageY + ")" ); //thirret te funksioni me siper
+// }
+
+
+
+
